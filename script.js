882fd68a380585c0ad8e156a26c28b31e6ab7d54
HadjMohamed <hadj.tmohamed@gmail.com>
2024-10-23 18:02:42 +0200

    Chatbot frontend added
    Chatbot frontend + better portfolio description

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let menu=document.querySelector('#menu-icon');
-let links=document.querySelector('.links');
+// Responsivity block
+let menu = document.querySelector('#menu-icon');
+let links = document.querySelector('.links');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
@@ -11,14 +12,70 @@ window.onscroll = () => {
     links.classList.remove('active');
 }
 
-
-
-
 // Typing text
-
-const typed= new Typed('.typing-text',{
-    strings:['a data engineer','specialized in Python','graduated in 2024'],
+const typed = new Typed('.typing-text', {
+    strings: ['a data engineer', 'specialized in Python', 'graduated in 2024'],
     typeSpeed: 50,
     backSpeed: 90,
     loop: true,
-})
+});
+
+// Chatbot modal
+function openModal() {
+    document.getElementById("chatbot-modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("chatbot-modal").style.display = "none";
+}
+
+function sendMessage() {
+    const userInput = document.getElementById("user-input").value;
+    if (userInput.trim() === "") return;
+
+    // Display user's message
+    const messagesContainer = document.getElementById("chatbot-messages");
+    const userMessageDiv = document.createElement("div");
+    userMessageDiv.className = "message user";
+    userMessageDiv.innerText = userInput;
+    messagesContainer.appendChild(userMessageDiv);
+
+
+    // Scroll to the bottom
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+    // clearing input
+    document.getElementById("user-input").value = "";
+
+    // fetching the question
+    fetch("http://127.0.0.1:5000/chatbot", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: userInput }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the response
+        const botMessageDiv = document.createElement("div");
+        botMessageDiv.className = "message bot";
+        botMessageDiv.innerText = data.response;
+        messagesContainer.appendChild(botMessageDiv);
+
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    })
+    .catch(error => {
+        console.error("Erreur:", error);
+    });
+}
+
+// Keyboard shortcuts
+document.getElementById('send-btn').addEventListener('click', sendMessage);
+
+document.getElementById('user-input').addEventListener('keydown', function (event) {
+    if ((event.key === 'Enter' && !event.shiftKey) && !(event.ctrlKey || event.metaKey)) {
+        event.preventDefault();  // Empêche le retour à la ligne
+        sendMessage();  // Envoie le message
+    }
+});
